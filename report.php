bae18f0569ed24e26b70b9b33102b326457b322d
Shweta-21 <51434784+Shweta-21@users.noreply.github.com>
2019-07-17 16:52:08 +0530

    test request method

--- report.php
+++ report.php
@@ -4,6 +4,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 //Process only when method is POST
 if($method == "POST"){
+    print_r('post method');
     $requestBody = file_get_contents('php://input');
     $json = json_decode($requestBody);
 
