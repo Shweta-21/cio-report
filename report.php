19b73613e248a5c5f8940bca01da07e5532a5478
Shweta-21 <51434784+Shweta-21@users.noreply.github.com>
2019-07-18 12:46:10 +0530

    report data added

--- report.php
+++ report.php
@@ -7,32 +7,29 @@ if($method == "POST"){
     $requestBody = file_get_contents('php://input');
     $json = json_decode($requestBody);
 
-    $text = $json->result->parameters->text;
-    echo ($text);
+    $event = $json->event_type;
+    $campaign = $json->data->campaign_name;
 
-    switch($text) {
-        case 'hi':
-            $speech = "Hi, Nice to meet you";
-            break;
+//     switch($text) {
+//         case 'hi':
+//             $speech = "Hi, Nice to meet you";
+//             break;
 
-        case 'bye':
-            $speech = "Bye, Good Night";
-            break;
+//         case 'bye':
+//             $speech = "Bye, Good Night";
+//             break;
         
-        case 'anything':
-            $speech = "Yes, you can type anything here.";
-            break;
-        default:
-            $speech = "sorry, I ddint get that, Please ask me something else.";
-            break;
-    }
+//         case 'anything':
+//             $speech = "Yes, you can type anything here.";
+//             break;
+//         default:
+//             $speech = "sorry, I ddint get that, Please ask me something else.";
+//             break;
+//     }
     $response = new \stdClass();
-    $response->speech = "";
-    $response->displayText = "";
-    $response->text = "";
-    $response->source = "webhook";
+    $response->event = $event;
+    $response->campaign = $campaign;
     echo json_encode($response);
-    echo ('echoing data2');
 }
 else{
     echo "method not allowed";
