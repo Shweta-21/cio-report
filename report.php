dc4eb6a3513f56e62d4818cd54abbb74af76b54f
Shweta Sharma <shweta.s@endurance.com>
2019-07-19 18:31:55 +0530

    modified

--- report.php
+++ report.php
@@ -1,44 +1,23 @@
 <?php
 
-$json = file_get_contents('php://input');
-$data = json_decode($json,1);
+$method = $_SERVER['REQUEST_METHOD'];
 
-$data_static = "Sab lemon hai...";
-echo "Static data : " . $data_static;
+//Process only when method is POST
+if($method == "POST"){
+    $requestBody = file_get_contents('php://input');
+    $json = json_decode($requestBody);
 
-print_r($data);
+    $response = new \stdClass();
+    $response->campaign_id = $json->data->campaign_id;
+    $response->campaign_name = $json->data->campaign_name;
+    $response->event_type = $json->event_type;
+    $res =  json_encode($response);
+    echo $res;
 
-$fWrite = fopen("log.txt","a");
-$wrote = fwrite($fWrite, $data);
-
-$fWriteS = fopen("log_static.txt","a");
-$wroteS = fwrite($fWriteS, $data_static);
-
-fclose($fWrite);
-fclose($fWriteS);
-
-
-
-// $method = $_SERVER['REQUEST_METHOD'];
-
-// //Process only when method is POST
-// if($method == "POST"){
-//     $requestBody = file_get_contents('php://input');
-//     $json = json_decode($requestBody);
-
-//     $event = $json->event_type;
-//     $campaign = $json->data->campaign_name;
-
-//     $response = new \stdClass();
-//     $response->event = $event;
-//     $response->campaign = $campaign;
-//     $res =  json_encode($response);
-//     echo $res;
-
-//     mail("shweta.s@endurance.com","CIO-Report",$res);    
-// }
-// else{
-//     echo "method not allowed";
-// }
+    mail("shweta.s@endurance.com","CIO-Report",$res);    
+}
+else{
+    echo "method not allowed";
+}
 
 ?>
