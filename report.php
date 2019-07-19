d502c86815f28c285bb2d0722d6a7b670d817b58
Shweta Sharma <shweta.s@endurance.com>
2019-07-19 16:26:34 +0530

    removed code

--- report.php
+++ report.php
@@ -1,25 +1,25 @@
 <?php
 
-$method = $_SERVER['REQUEST_METHOD'];
+// $method = $_SERVER['REQUEST_METHOD'];
 
-//Process only when method is POST
-if($method == "POST"){
-    $requestBody = file_get_contents('php://input');
-    $json = json_decode($requestBody);
+// //Process only when method is POST
+// if($method == "POST"){
+//     $requestBody = file_get_contents('php://input');
+//     $json = json_decode($requestBody);
 
-    $event = $json->event_type;
-    $campaign = $json->data->campaign_name;
+//     $event = $json->event_type;
+//     $campaign = $json->data->campaign_name;
 
-    $response = new \stdClass();
-    $response->event = $event;
-    $response->campaign = $campaign;
-    $res =  json_encode($response);
-    echo $res;
+//     $response = new \stdClass();
+//     $response->event = $event;
+//     $response->campaign = $campaign;
+//     $res =  json_encode($response);
+//     echo $res;
 
-    mail("shweta.s@endurance.com","CIO-Report",$res);    
-}
-else{
-    echo "method not allowed";
-}
+//     mail("shweta.s@endurance.com","CIO-Report",$res);    
+// }
+// else{
+//     echo "method not allowed";
+// }
 
 ?>
