16176c8b87206de2272748c2f3b7e78ab0cbe327
Shweta Sharma <shweta.s@endurance.com>
2019-07-19 18:37:10 +0530

    modified

--- report.php
+++ report.php
@@ -10,7 +10,11 @@ if($method == "POST"){
     $response = new \stdClass();
     $response->campaign_id = $json->data->campaign_id;
     $response->campaign_name = $json->data->campaign_name;
+    $response->customer_id = $json->data->customer_id;
+    $response->customer_email = $json->data->email_address;
+    $response->email_name = $json->data->message_name;
     $response->event_type = $json->event_type;
+    $response->timestamp = $json->timestamp;
     $res =  json_encode($response);
     echo $res;
 
